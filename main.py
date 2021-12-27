bb3253d3940fde3a6ccf0aa59db4e751dd2ff05c
KentStuder22 <77794005+KentStuder22@users.noreply.github.com>
2021-12-27 15:55:02 -0600

    Dec 27, 2021 updates

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from os import path
 import json
+from posixpath import join
+import config
 import pymongo
 import tornado.ioloop
 import tornado.web
@@ -7,30 +10,49 @@ from bson import ObjectId
 from pymongo.mongo_client import MongoClient
 from sshtunnel import SSHTunnelForwarder
 
+#Settings for Mongo Server
+MONGODB_SERVER = '119.45.163.114'
+MONGO_USER = 'kent'
+MONGO_PASSWORD = 'kent'
+MONGODB_PORT = 27017
+MONGODB_DB = 'TweetScraper'
+
+server = SSHTunnelForwarder(
+    MONGODB_SERVER,
+    ssh_username=MONGO_USER,
+    ssh_password=MONGO_PASSWORD,
+    remote_bind_address=('127.0.0.1', MONGODB_PORT),
+)
+
+server.start()
+connection = pymongo.MongoClient('127.0.0.1', server.local_bind_port)
+db = connection[MONGODB_DB]
+
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        tweets = self.settings["db"]["tweet"].find()
-        self.render("index.html", tweets = tweets)
+        datetimes = []
+        texts = []
+        retweets = []
+        favorites = []
+        urls = []
+        
+        limit = 20
+        i = 1
+        for tweet in db["tweetday"].find():
+            datetimes.append(tweet['created_at'])
+            texts.append(tweet['text'])
+            retweets.append(tweet['retweet_cou'])
+            favorites.append(tweet['like_count'])
+            urls.append(tweet['url'])
+            if i == limit:
+                break
+            else:
+                i += 1
+        self.render("index.html", datetimes = datetimes, texts = texts, retweets = retweets, favorites = favorites, urls = urls)
+
 
 def setUpApp(): 
-    #Settings for Mongo Server
-    MONGODB_SERVER = '119.45.163.114'
-    MONGO_USER = 'kent'
-    MONGO_PASSWORD = 'kent'
-    MONGODB_PORT = 27017
-    MONGODB_DB = 'TweetScraper'
-
-    server = SSHTunnelForwarder(
-        MONGODB_SERVER,
-        ssh_username=MONGO_USER,
-        ssh_password=MONGO_PASSWORD,
-        remote_bind_address=('127.0.0.1', MONGODB_PORT),
-    )
-
-    server.start()
-
-    connection = pymongo.MongoClient('127.0.0.1', server.local_bind_port)
-    db = connection[MONGODB_DB]
 
     app = tornado.web.Application(
         [
@@ -41,6 +63,7 @@ def setUpApp():
     return app
 
 if __name__=="__main__":
+    print(config.__version__)
     app = setUpApp()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
